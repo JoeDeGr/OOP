6f65c03d781b254e095874f5d3daaf98390bef89
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-08-31 13:44:36 -0400

    added OO example.

--- objectOriented.js
+++ objectOriented.js
@@ -2,6 +2,15 @@
 
 
 const employee = {
+    salary: 45000,
+    totalOvertime: 10,
+    overtimeHourlyRate: function () {
+        return this.salary / 2000 * 1.5
+    },
+    totalCompensation: function () {
+        return this.salary + (this.totalOvertime * this.overtimeHourlyRate());
+    }
+};
 
-}
+employee.totalCompensation();
 
